fdb5857d444a56d42511090809276d96929ade10
Karl Penzhorn <karlpenzhorn@gmail.com>
2018-11-30 16:22:45 +0000

    Linting, and (most) buttons

--- App.js
+++ App.js
@@ -4,29 +4,41 @@
  */
 
 import React from 'react';
-import { Container, Header, Input, Keypad, ButtonRow } from './components';
+import {
+  Container, Header, Input, Keypad, ButtonRow
+} from './components';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {number:123456};
+    this.state = { number: 123456 };
 
     // This binding is necessary to make `this` work in the callback
     this.press = this.press.bind(this);
   }
+
   press = (value) => {
-    alert(value);
+    let { number } = this.state;
+    if (value === 'Clear') {
+      number = Math.floor(number / 10);
+      this.setState({ number });
+    } else if (value != 'Go') {
+      number += value;
+      this.setState({ number });
+    }
   };
+
   render() {
+    const { number } = this.state;
     return (
       <Container>
         <Header>Prime Components</Header>
-        <Input>{this.state.number}</Input>
+        <Input>{number}</Input>
         <Keypad>
-          <ButtonRow func={this.press} keys={['1','2','3']} />
-          <ButtonRow func={this.press} keys={['4','5','6']} />
-          <ButtonRow func={this.press} keys={['7','8','9']} />
-          <ButtonRow func={this.press} keys={['0','Clear','Go']} />
+          <ButtonRow func={this.press} keys={['1', '2', '3']} />
+          <ButtonRow func={this.press} keys={['4', '5', '6']} />
+          <ButtonRow func={this.press} keys={['7', '8', '9']} />
+          <ButtonRow func={this.press} keys={['0', 'Clear', 'Go']} />
         </Keypad>
       </Container>
     );
