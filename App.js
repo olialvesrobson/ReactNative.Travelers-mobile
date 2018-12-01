572276eb9e7bcdb931c93e319230b45e253bef16
Karl Penzhorn <karlpenzhorn@gmail.com>
2018-12-01 18:14:02 +0000

    Class property state, and cleanup

--- App.js
+++ App.js
@@ -8,13 +8,15 @@ import {
   Container, Header, Input, Processing, Keypad, ButtonRow
 } from './components';
 
+/*
 type State = {
   number: number;
   nums: Array;
   running: boolean;
 };
+*/
 
-class App extends Component<{}, State> {
+class App extends Component {
   state = {
     number: 123456,
     factors: [],
@@ -29,16 +31,14 @@ class App extends Component<{}, State> {
       factors.push({ key: '2' });
       num /= 2;
     }
-    let i;
-    for (i = 3; i <= Math.floor(Math.sqrt(num)); i += 2) {
+    let i; for (i = 3; i <= Math.floor(Math.sqrt(num)); i += 2) {
       while (num % i === 0) {
         factors.push({ key: `${i}` });
         num /= i;
       }
     }
     if (num > 1) { factors.push({ key: `${num}` }); }
-    this.setState({ running: false });
-    this.setState({ factors });
+    return factors;
   };
 
   press = (value) => {
@@ -51,7 +51,9 @@ class App extends Component<{}, State> {
       this.setState({ number });
     } else if (value === 'Go') {
       this.setState({ processing: true });
-      this.getPrimes(number);
+      let factors = this.getPrimes(number);
+      this.setState({ running: false });
+      this.setState({ factors });
     } else if (value === 'Back') {
       this.setState({ processing: false });
     }
